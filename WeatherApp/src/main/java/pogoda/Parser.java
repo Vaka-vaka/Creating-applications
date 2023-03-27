1259434ec795a58c16acc691c864165ae2e95ba9
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2023-03-27 16:17:11 +0300

    update class Parser

--- WeatherApp/src/main/java/pogoda/Parser.java
+++ WeatherApp/src/main/java/pogoda/Parser.java
@@ -10,7 +10,6 @@ package src.main.java.pogoda;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.net.URL;
@@ -20,14 +19,13 @@ import java.util.regex.Pattern;
 public class Parser {
     private static Document getPage() throws IOException {
         String url = "https://www.gismeteo.ua/ua/";
-        Document page = Jsoup.parse(new URL(url), 3000);
-        return page;
+        return Jsoup.parse(new URL(url), 3000);
+//        Document page = Jsoup.parse(new URL(url), 3000);
+//        return page;
     }
 
     //Нд, 26 березня, 22:15
-    //26 березня
-    //
-    private static Pattern pattern = Pattern.compile("\\d{2}\\s\\W{7}");
+    private static final Pattern pattern = Pattern.compile("\\d{2}\\s\\W{7}");
 
     private static String getDateFromString(String stringDate) throws Exception {
         Matcher matcher = pattern.matcher(stringDate);
@@ -35,6 +33,15 @@ public class Parser {
             return matcher.group();
         }
         throw new Exception("Can't extract date from string!");
+    }
+
+    private static String printFourValues(String values, int index) {
+        String string = "";
+        String[] arrayString = values.split(" ");
+        for (int i = 0; i < arrayString.length; i++) {
+            string = arrayString[index];
+        }
+        return string;
     }
 
     /**
@@ -47,9 +54,13 @@ public class Parser {
         //css query language
         Element divWth = page.select("div[class=frame-now]").first();
         String names = divWth.select("div[class=current-time]").text();
-        Elements label = divWth.select("div[class=item-label]");
-        Elements value = divWth.select("div[class=item-value]");
+        //Elements label = divWth.select("div[class=item-label]");
+        String value = divWth.select("div[class=item-value]").text();
         String date = getDateFromString(names);
-        System.out.println(date + " Температура    Вітер    Тиск    Вологість    Вода");
+        System.out.println("Погода Gismeteo " + date + "\nТемпература = " + printFourValues(value, 0)
+                + ";\nВітер = " + printFourValues(value, 2) + " м/с"
+                + ";\nТиск = " + printFourValues(value, 8) + " мм рт. ст."
+                + ";\nВологість = " + printFourValues(value, 14) + " %"
+                + ";\nВода = " + printFourValues(value, 18) + ".");
     }
 }
