9e6587306f3beb913b1cf5f891750226c845f9f2
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2023-06-13 10:08:35 +0300

    create class TicTacToe

---
+++ basic_tic_tac_toe_game/src/main/java/TicTacToe.java
@@ -0,0 +1,13 @@
+/**
+ * Game tic tac toe AI
+ * Игра крестики-нолики на доске 3x3
+ *
+ * @author Valentyn Mozul
+ * @version 3.0 from 10.06.2023
+ */
+
+package src.main.java;
+
+public class TicTacToe {
+
+}
