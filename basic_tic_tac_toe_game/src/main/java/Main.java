01a96a3b0a7f7130186c10d79a83281bd283469e
Valentin <84026149+Vaka-vaka@users.noreply.github.com>
2023-06-13 10:03:49 +0300

    create class Main

---
+++ basic_tic_tac_toe_game/src/main/java/Main.java
@@ -0,0 +1,20 @@
+/**
+ * Game tic tac toe AI
+ * Игра крестики-нолики на доске 3x3
+ *
+ * @author Valentyn Mozul
+ * @version 3.0 from 10.06.2023
+ */
+
+package src.main.java;
+
+public class Main {
+    /**
+     * Program entry point.
+     *
+     * @param args command Line arguments
+     */
+    public static void main(String[] args) {
+
+    }
+}
